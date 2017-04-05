69cb38d50d05fc0a103c1669605b7c6834a0ed4d
Adrian Pearman <adrianpearman12@gmail.com>
2017-04-05 14:46:59 -0400

    update to file data

--- exercise2.rb
+++ exercise2.rb
@@ -1,15 +1,56 @@
 class Book
-  #will showcase all the books currently available to be lent out
+  #Class variables and methods
   @@on_shelf = []
-  #will showcase all the books that are being borrowed
   @@on_loan = []
+  @@time = Time.new(2017, 03, 17)
 
+  def self.create (title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
+    create = []
+    create.push(title, author, isbn)
+    @@on_shelf << create
+    return create
+  end
+
+  def self.current_due_date
+    # return Time.now
+    # puts base_time
+    # base_time = Time.new(2017, 03, 17)
+    base_time = @@time
+    due_date = base_time + 5184000
+    # 5184000 = refers to the amount of seconds
+    puts due_date
+  end
+
+  def self.overdue
+    base_time = @@time
+    due_date = base_time + 5184000
+    if Time.now > due_date
+      return true
+    else
+      print "Nothing is Overdue"
+      print "\n"
+    end
+  end
+
+  def self.browse
+    return
+  end
+
+  def self.available
+    return @@on_shelf
+  end
+
+  def self.borrowed
+    return @@on_loan
+  end
+  #Instance variables and methods
   attr_accessor :due_date
 
-  @Time = Time.now
-
-  def initialize (name, author, isbn = "n/a")
-    @name = name
+  def initialize(title, author, isbn)
+    @title = title
     @author = author
     @isbn = isbn
   end
@@ -22,43 +63,14 @@ class Book
 
   end
 
-  def lent_out?(book)
-    lent_out = @@on_shelf
-    lent_out.each do |lent|
-      if lent == book
-        puts "#{book} is not available."
-      else
-        puts "#{book} is available!"
-      end
-    end
-  end
-
-  def self.create(name, author, id)
-    new_book = Book.new(name, author, id)
-     @@on_shelf << new_book
-    return new_book
-  end
-
-  def self.current_due_date
-
-  end
-
-  def self.overdue_books
-
-  end
-
-  def self.browse
-
-  end
-
-  def available
-    available = @@on_shelf
-    return available.inspect
-  end
-
-  def self.borrowed
-
-  end
-
-
+#   def lent_out
+#     if  ==
+#       return true
+#     else
+#       return false
+#     end
+#   end
 end
+
+# book = Book.new
+# book.create("afafaf","sdfafsd",234234)
